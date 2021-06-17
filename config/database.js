13051748219ce63d2190e8b26b58ceca8d5a30b1
linh <linh.ha.nguyen20@gmail.com>
2021-06-17 11:05:55 +0700

    config heroku env

--- config/database.js
+++ config/database.js
@@ -23,20 +23,6 @@ module.exports = {
     }
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false,
-    define: {
-      "timestamps": false
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
+    use_env_variable: process.env.DB_URL
   }
 }
